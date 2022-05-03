34317af5c007615f9f91c5dbf5978e66806d456e
Gregory Josephs <greg.josephs@gmail.com>
2022-05-03 10:15:52 -0400

    Added css and started porting home content

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>
-            Hello World!
+        <div className="centered">
+             <h1>Gregory Josephs: Full Stack Developer In Training</h1>
+             <div class="centered">
+            <h1>About Me</h1>
+          </div>
+          <div class="full-width-text">
+
+            <p>Driven by a passion for logic and process, I am a full-stack web developer in training, currently enrolled in the Professional Certificate in Coding course from MIT XPro.</p>
+
+            <p>While I started writing HTML in the late 90s, my path to coding-as-a-career has been happily circuitous. While I am proud to have worked in the recreation department at MIT since 2005, I am also a published author of two novels, and have helped to feed my community as a weekly volunteer farm-hand since 2016.</p>
+            
+            <p>These diverse experiences outside the realm of coding have given me deep insight into the logic and processes that go into solving both tangible and intangible problems. They allow me a wealth of unique real-world perspective that I bring to each coding challenge I face.</p>
+            
+            <p>In coding, as in life, I believe there is always room for improvement. The first way of doing something is rarely the best way, and I’m constantly on the lookout for a better solution.</p>
+            
+            <p>I currently reside in Maynard, Massachusetts (just a stone’s throw from Boston) with my husband, two cats, and an Australian Cattle Dog.
+            </p>
+
+            <p>Take a look at all the projects in my portfolio by visiting the <a href="projects.html">projects page</a>, or take a look at the three most-recent below.</p>
+
+            <p>Find me on <a href="https://github.com/gjosephs84" target="_blank">GitHub</a>, <a href="https://linkedin.com/in/gjosephs" target="_blank">LinkedIn</a>, <a href="https://twitter.com/gjosephs" target="_blank">Twitter</a>, or reach me via email at <a href="mailto:greg.josephs@gmail.com">greg.josephs@gmail.com</a></p>
+          </div>
         </div>
     )
 }
