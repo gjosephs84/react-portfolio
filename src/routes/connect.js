47f0fac1e7ab7c5fa5c2d9196d8e5ad5aefeabd1
Gregory Josephs <greg.josephs@gmail.com>
2022-05-03 09:35:58 -0400

    Created home, projects, connect

---
+++ src/routes/connect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Connect() {
+    return (
+        <div>
+            Hello Connect!
+        </div>
+    )
+}
+
+export default Connect;
---
+++ src/routes/projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Projects() {
+    return (
+        <div>
+            Hello Projects!
+        </div>
+    )
+}
+
+export default Projects;
